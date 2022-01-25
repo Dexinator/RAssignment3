c3e3ca1ad34964b36a45a14a0ca0ba5a836219e0
Jorge Badillo <badillo.delgado.jorge@gmail.com>
2022-01-24 23:49:15 -0600

    Cleared testing and experimental variables, on this task I've used order function to break ties, this was better than in Task 1 because my resultant vector is already ordered with the 2 critera (main and tie breaker). Finally added conditions to make possible working with the words "best" and "worst"

--- Third Task.R
+++ Third Task.R
@@ -1,64 +1,43 @@
-##Cleaned test and experimental variables. Function best inputs State Abrv. and a disease,
-##it will return the name of the hospital with the lowest given disease mortality rate
+## Function rankhospital inputs State Abrv., a disease and the rank of hospital,
+##it will return the name of the hospital with the given rank using disease mortality rate
 ##in that state. It will return error mssgs if inputs are not well written as requested.
-file2 <- read.csv("outcome-of-care-measures.csv")
-column<-NULL
-DIS_HA<-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
-DIS_HF<-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
-DIS_PN<-"Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
-outcome<-"heart failure"
-state<-"TX"
-if (outcome=="heart attack"){column<-DIS_HA}else if(outcome=="heart failure"){column<-DIS_HF}else if(outcome=="pneumonia"){column<-DIS_PN}
-reduced_data<-subset(file2, State==state, select=c("Hospital.Name",column))
-reduced_data[order(as.numeric(reduced_data[,2])),]
-reduced_data[order(reduced_data[,2]),]
-as.numeric(reduced_data[,2])
-reduced_data2<-reduced_data
-reduced_data2[,2]<-as.numeric(reduced_data2[,2])
-reduced_data2[order(reduced_data2[,2],na.last = NA),]
-
-
-
-?order
-Hospital_RATE<-reduced_data[which(reduced_data[,2] == min(reduced_data[,2], na.rm = TRUE)),]
-Hospital_NAME<-Hospital_RATE[,1]
-if (length(Hospital_NAME)>1){
-  Hospitals<-order(Hospital_NAME)
-  Hospitals[1]
-}else{
-  Hospital_NAME}
-
-
-rankhospital <- function(state, outcome) {
+rankhospital <- function(state, outcome, num = "best") {
   ## Read outcome data
   file <- read.csv("outcome-of-care-measures.csv")
   ## Check that state and outcome are valid
   
   if (isTRUE(outcome!="heart attack" & outcome!="heart failure"& outcome!="pneumonia")&isTRUE(any(file$State==state)==FALSE)){
-  return ("invalid state and invalid outcome")  
+  stop ("invalid state and invalid outcome")  
   }
   
   if (any(file$State==state)==FALSE){
-    return ("invalid state")
+    stop ("invalid state")
   }
   if (outcome!="heart attack" & outcome!="heart failure"& outcome!="pneumonia"){
-    return("invalid outcome")
+    stop("invalid outcome")
   }
   
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
+  ## Return hospital name in that state with the given rank
+  ## 30-day death rate
   column<-NULL
   DIS_HA<-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
   DIS_HF<-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
   DIS_PN<-"Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
   if (outcome=="heart attack"){column<-DIS_HA}else if(outcome=="heart failure"){column<-DIS_HF}else if(outcome=="pneumonia"){column<-DIS_PN}
   reduced_data<-subset(file, State==state, select=c("Hospital.Name",column))
-  Hospital_RATE<-reduced_data[which(reduced_data[,2] == min(reduced_data[,2], na.rm = TRUE)),]
-  Hospital_NAME<-Hospital_RATE[,1]
-  if (length(Hospital_NAME)>1){
-    Hospitals<-order(Hospital_NAME)
-    Hospitals[1]
-  }else{
-    Hospital_NAME}
+  reduced_data[,2]<-as.numeric(reduced_data[,2])
+  ord_red_data<-reduced_data[order(reduced_data[,2],na.last = NA,reduced_data[,1]),]
+  if (num=="best"){
+    ord_red_data[1,1]
+  }else if (num=="worst"){
+    tail(ord_red_data[,1],n=1)
+  }else if (num>0 & num<length(reduced_data[,1])){
+  ord_red_data[num,1]
+  }
+  else {
+    "NA"
+  }
+  
 }
-best("TX","heart attack")
+rankhospital("TX","heart failure",2000)
+rankhospital("TX", "heart failure", 4)
